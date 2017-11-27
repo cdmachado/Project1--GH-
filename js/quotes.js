150d8237e47f04f5d57390aa49f5400ced5b16c8
cdmachado <cdmachado23@gmail.com>
2017-11-27 18:59:56 +0300

    First day of work
    Page shows random quotes but when you refresh it and not when you click
    in the button. Also, there are some problem with the optional object
    properties

---
+++ js/quotes.js
@@ -0,0 +1,29 @@
+// Famous Quotes
+
+var quotes = [
+  {
+    quote: 'Success is walking from failure to failure with no loss of enthusiasm.',
+    source: 'Winston Churchill'
+  },
+  {
+    quote: 'Try not to become a man of success. Rather become a man of value.',
+    source: 'Albert Einstein'
+  },
+  {
+    quote: 'You can avoid reality, but you cannot avoid the consequences of avoiding reality.',
+    source: 'Ayn Rand',
+    citation: '"Ethics in Our Time" symposium',
+    year: 1961
+  },
+  {
+    quote: 'Do, or do not. There is no "try".',
+    source: 'Yoda',
+    citation: 'The Empire Strikes Back',
+    year: 1980
+  },
+  {
+    quote: 'There are no facts, only interpretations.',
+    source: 'Friedrich Nietzsche',
+    year: 1886
+  }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,27 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var randomNumber;
+var randomQuote;
+
+function getRandomQuote() {
+  randomNumber = Math.floor( Math.random() * 6);
+  randomQuote = quotes[randomNumber];
+  return randomQuote;
+}
+
+function printQuote() {
+  var randomQuote2 = getRandomQuote();
+  var quotePrint = '<p class="quote">' + randomQuote2.quote + '</p>';
+  quotePrint += '<p class="source">' + randomQuote2.source;
+  quotePrint += '<span class="citation">' + randomQuote2.citation + '</span>';
+  quotePrint += '<span class="year">' + randomQuote2.year + '</span></p>';
+  return quotePrint;
+}
+
+function print(message) {
+var quoteBox = document.getElementById('quote-box');
+quoteBox.innerHTML = message;
+}
+
+print(printQuote());
