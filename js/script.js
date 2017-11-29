ce162d1e7d8ae1d3b8d7849fceb1fc29cc7eba4a
cdmachado <cdmachado23@gmail.com>
2017-11-29 15:18:20 +0300

    2nd change
    Problem regarding the optional properties solved

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
 var randomNumber;
+var timeoutID;
 
 function getRandomQuote() {
   randomNumber = Math.floor( Math.random() * 6);
@@ -13,7 +15,13 @@ function printQuote() {
   var randomQuote = getRandomQuote();
   var quotePrint = '<p class="quote">' + randomQuote.quote + '</p>';
   quotePrint += '<p class="source">' + randomQuote.source;
-  quotePrint += '<span class="citation">' + randomQuote.citation + '</span>';
-  quotePrint += '<span class="year">' + randomQuote.year + '</span></p>';
+    if ( randomQuote.citation !== '') {
+        quotePrint += '<span class="citation">' + randomQuote.citation + '</span>';
+    }
+    if ( randomQuote.year !== '') {
+        quotePrint += '<span class="year">' + randomQuote.year + '</span></p>';
+    }
   document.getElementById('quote-box').innerHTML = quotePrint;
 }
+
+timeoutID = window.setTimeout(printQuote, 3000);
