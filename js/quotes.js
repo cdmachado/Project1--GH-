f1790fb818673da1920b9ae18143910e1022c0b7
cdmachado <cdmachado23@gmail.com>
2017-12-01 12:00:51 +0300

    Project finished
    All bugs were fixed for the "Exceed Expectations" grade

--- js/quotes.js
+++ js/quotes.js
@@ -5,30 +5,35 @@ var quotes = [
     quote: 'Success is walking from failure to failure with no loss of enthusiasm.',
     source: 'Winston Churchill',
     citation: '',
-    year: ''
+    year: '',
+    tags: 'Success'
   },
   {
     quote: 'Try not to become a man of success. Rather become a man of value.',
     source: 'Albert Einstein',
     citation: '',
-    year: ''
+    year: '',
+    tags: 'Success'
   },
   {
     quote: 'You can avoid reality, but you cannot avoid the consequences of avoiding reality.',
     source: 'Ayn Rand',
     citation: '"Ethics in Our Time" symposium',
-    year: 1961
+    year: 1961,
+    tags: 'History'
   },
   {
     quote: 'Do, or do not. There is no "try".',
     source: 'Yoda',
     citation: 'The Empire Strikes Back',
-    year: 1980
+    year: 1980,
+    tags: 'Movies'
   },
   {
     quote: 'There are no facts, only interpretations.',
     source: 'Friedrich Nietzsche',
     citation: '',
-    year: 1886
+    year: 1886,
+    tags: 'Literature'
   }
 ];
--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//Variables
 
 var timeoutID;
 var intervalID;
+
+//Random element generating functions
 
 function getRandomQuote() {
   var randomNumber = Math.floor( Math.random() * 5);
@@ -23,6 +26,8 @@ function randomColor() {
   return color;
 }
 
+//Main function
+
 function printQuote() {
   var rgbcolor = randomColor();
   var randomQuote = getRandomQuote();
@@ -34,13 +39,15 @@ function printQuote() {
     if ( randomQuote.year !== '') {
         quotePrint += '<span class="year">' + randomQuote.year + '</span></p>';
     }
+  quotePrint += '<p class="tags">' + randomQuote.tags + '</p>';
   document.getElementById('quote-box').innerHTML = quotePrint;
   document.getElementById('body').style.backgroundColor = rgbcolor;
 }
 
+//Time changing elements
+
 function timer() {
     timeoutID = window.setTimeout(printQuote, 1000);
 }
-
 
 intervalID = window.setInterval(timer, 5000);
